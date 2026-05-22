17ee44d0f0c2ed84602d443f364c23e8f49ae0db
prafful001 <kprafful60@gmail.com>
2026-05-22 13:20:51 +0530

    fixed api routes

--- milestone-1-student-api/app/__init__.py
+++ milestone-1-student-api/app/__init__.py
@@ -7,14 +7,14 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    # Database configuration
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///students.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
     from app.routes import student_bp
-    app.register_blueprint(student_bp)
+
+    app.register_blueprint(student_bp, url_prefix='/api/v1')
 
     with app.app_context():
         db.create_all()
